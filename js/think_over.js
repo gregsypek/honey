316b85c395ab6b8eb4e747e0c86a25901c22ea98
greg <greg.sypek@gmail.com>
2021-01-11 18:15:57 +0100

    add sticky navigation which showing up by IntersectionObserver

--- js/think_over.js
+++ js/think_over.js
@@ -19,3 +19,43 @@ dotsContainer.addEventListener('click', function (e) {
     .querySelector(`.recommend--${clicked.dataset.tab}`)
     .classList.add('recommend--active');
 });
+
+//sticky navigation starting in hero section
+// const navFirst = document.querySelector('.navigation__row--first');
+// const nav = document.querySelector('.navigation__row--second');
+// const navHeight = nav.getBoundingClientRect().height;
+
+// const stickyNav = function (entries) {
+//   const [entry] = entries;
+//   console.log(entry);
+
+//   if (!entry.isIntersecting) nav.classList.add('sticky');
+//   else nav.classList.remove('sticky');
+// };
+
+// const navFirstObserver = new IntersectionObserver(stickyNav, {
+//   root: null,
+//   threshold: 0,
+//   // rootMargin: `-${navHeight}px`,
+// });
+// navFirstObserver.observe(navFirst);
+
+//sticky navigation starting in info section
+const header = document.querySelector('.hero');
+const nav = document.querySelector('.navigation__row--second');
+const navHeight = nav.getBoundingClientRect().height;
+
+const stickyNav = function (entries) {
+  const [entry] = entries;
+  console.log(entry);
+
+  if (!entry.isIntersecting) nav.classList.add('sticky');
+  else nav.classList.remove('sticky');
+};
+
+const headerObserver = new IntersectionObserver(stickyNav, {
+  root: null,
+  threshold: 0,
+  rootMargin: `-${navHeight}px`,
+});
+headerObserver.observe(header);
