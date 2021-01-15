b6e1c276e4f140a25ea2f1f6bcaec21e42c68b71
greg <greg.sypek@gmail.com>
2021-01-15 21:09:30 +0100

    add media query to register.html and login.html

--- js/think_over.js
+++ js/think_over.js
@@ -76,7 +76,7 @@ const revealSection = function (entries, observer) {
 
 const sectionObserver = new IntersectionObserver(revealSection, {
   root: null,
-  threshold: 0.25,
+  threshold: 0.2,
 });
 
 allSections.forEach(function (section) {
