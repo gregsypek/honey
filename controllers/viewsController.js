5dd0f8fd5ca29825494f83924f9eea4e80644740
greg <greg.sypek@gmail.com>
2021-07-11 18:53:12 +0200

    fix indeds in pug files

--- controllers/viewsController.js
+++ controllers/viewsController.js
@@ -1,8 +1,17 @@
-exports.getOverview = (req, res) => {
+const Honey = require('../models/honeyModel');
+const catchAsync = require('../utils/catchAsync');
+
+exports.getOverview = catchAsync(async (req, res) => {
+  // Get honey data from collection
+  const honey = await Honey.find();
+  //Build template
+
+  //Render that template using honey data from collection
   res.status(200).render('overview', {
     title: 'Wszystkie miody',
+    honey,
   });
-};
+});
 exports.getHoney = (req, res) => {
   res.status(200).render('honey', {
     title: 'Miód malinowy',
