f6892f5e8fcc32752597c927d74f97832f9f2fa4
greg <greg.sypek@gmail.com>
2021-07-28 15:30:20 +0200

    image upload using multer part1

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,7 +1,47 @@
+const multer = require('multer');
+
 const User = require('../models/userModel');
 const catchAsync = require('../utils/catchAsync');
 const AppError = require('../utils/appError');
 const factory = require('./handlerFactory');
+//https://github.com/expressjs/multer
+
+/// THIS IS HOW REQ.FILE LOOKS:
+//  fieldname: 'photo',
+//   originalname: 'monica.jpg',
+//   encoding: '7bit',
+//   mimetype: 'image/jpeg',
+//   destination: 'public/src/images/users',
+//   filename: '7223b919e247d4d83342ecf080900f51',
+//   path: 'public/src/images/users/7223b919e247d4d83342ecf080900f51',
+//   size: 149257
+const multerStorage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, 'public/src/images/users'); // 'null' is an error place
+  },
+  filename: (req, file, cb) => {
+    const ext = file.mimetype.split('/')[1];
+    cb(null, `user-${req.user.id}-${Date.now()}.${ext}`);
+  },
+});
+
+const multerFilter = (req, file, cb) => {
+  if (file.mimetype.startsWith('image')) {
+    cb(null, true);
+  } else {
+    cb(
+      new AppError('Plik nie jest zdjęciem! Proszę wybrać zdjęcie', 400),
+      false
+    );
+  }
+};
+
+const upload = multer({
+  storage: multerStorage,
+  fileFilter: multerFilter,
+});
+
+exports.uploadUserPhoto = upload.single('photo'); //'photo' in middleware upload is the name of the field that is going to hold new file
 
 const filterObj = (obj, ...allowedFields) => {
   const newObj = {};
@@ -16,6 +56,8 @@ exports.getMe = (req, res, next) => {
   next();
 };
 exports.updateMe = catchAsync(async (req, res, next) => {
+  console.log(req.file);
+  console.log(req.body);
   // 1. Create error if user POSTs password data
   if (req.body.password || req.body.passwordConfirm) {
     return next(
