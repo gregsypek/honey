7cd3e9f4c6a914a386b4eabf6ce7eaf38c1873b2
greg <greg.sypek@gmail.com>
2021-07-28 15:56:41 +0200

    image upload using multer - part2 saving image name to database

--- controllers/userController.js
+++ controllers/userController.js
@@ -56,13 +56,11 @@ exports.getMe = (req, res, next) => {
   next();
 };
 exports.updateMe = catchAsync(async (req, res, next) => {
-  console.log(req.file);
-  console.log(req.body);
   // 1. Create error if user POSTs password data
   if (req.body.password || req.body.passwordConfirm) {
     return next(
       new AppError(
-        'This route is not for password updates.Please use/ updateMyPassword'
+        'This route is not for password updates. Please use/ updateMyPassword'
       ),
       400
     );
@@ -70,6 +68,7 @@ exports.updateMe = catchAsync(async (req, res, next) => {
   // 2. Filtered out unwanted fields names that are not allowed to be updated
 
   const filteredBody = filterObj(req.body, 'name', 'email'); // the rest throw away like 'role' to prevent changing to admin
+  if (req.file) filteredBody.photo = req.file.filename;
 
   // 3. Update user document
   const updatedUser = await User.findByIdAndUpdate(req.user.id, filteredBody, {
