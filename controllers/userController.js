39cb5d6af05bfa71d2ee9a9c8b5e4aeb316a9bc5
greg <greg.sypek@gmail.com>
2021-07-28 17:36:16 +0200

    image upload using multer - part3 resizing images

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,5 +1,5 @@
 const multer = require('multer');
-
+const sharp = require('sharp');
 const User = require('../models/userModel');
 const catchAsync = require('../utils/catchAsync');
 const AppError = require('../utils/appError');
@@ -15,15 +15,17 @@ const factory = require('./handlerFactory');
 //   filename: '7223b919e247d4d83342ecf080900f51',
 //   path: 'public/src/images/users/7223b919e247d4d83342ecf080900f51',
 //   size: 149257
-const multerStorage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    cb(null, 'public/src/images/users'); // 'null' is an error place
-  },
-  filename: (req, file, cb) => {
-    const ext = file.mimetype.split('/')[1];
-    cb(null, `user-${req.user.id}-${Date.now()}.${ext}`);
-  },
-});
+
+// const multerStorage = multer.diskStorage({
+//   destination: (req, file, cb) => {
+//     cb(null, 'public/src/images/users'); // 'null' is an error place
+//   },
+//   filename: (req, file, cb) => {
+//     const ext = file.mimetype.split('/')[1];
+//     cb(null, `user-${req.user.id}-${Date.now()}.${ext}`);
+//   },
+// });
+const multerStorage = multer.memoryStorage();
 
 const multerFilter = (req, file, cb) => {
   if (file.mimetype.startsWith('image')) {
@@ -42,6 +44,22 @@ const upload = multer({
 });
 
 exports.uploadUserPhoto = upload.single('photo'); //'photo' in middleware upload is the name of the field that is going to hold new file
+
+//sharp.pixelplumbing.com/api-resize
+https: exports.resizeUserPhoto = (req, res, next) => {
+  if (!req.file) return next();
+
+  req.file.filename = `user-${req.user.id}-${Date.now()}.jpeg`;
+
+  //sharp creates an object
+  sharp(req.file.buffer)
+    .resize(500, 500)
+    .toFormat('jpeg')
+    .jpeg({ quality: 90 })
+    .toFile(`public/src/images/users/${req.file.filename}`);
+
+  next();
+};
 
 const filterObj = (obj, ...allowedFields) => {
   const newObj = {};
